81b4ed82d12ec546da925eb09c7e14c00d9166e1
vueexcel <vueexcel@excellencetechnologies.in>
2020-02-21 17:26:59 +0530

    audio test commit

---
+++ src/components/Audio.vue
@@ -0,0 +1,100 @@
+<template>
+  <div>
+      <b-row class="mt-5" align-h="center">
+      <b-col cols="4">
+        <b-form-file v-model="audioFile" @change="getJsonFile"></b-form-file>
+      </b-col>
+    </b-row>
+    <b-row align-h="center">
+      <b-col cols="3" align-self="center">
+        <div ref="waveform" class="mt-5"></div>
+        <div v-if="timestamp.length">
+            <b-icon v-if="play" icon="pause-fill" variant="primary" class="cursor" style="width: 50px; height: 50px;" @click="playPauseFunction"></b-icon>
+            <b-icon v-else icon="play-fill" variant="success" class="cursor" style="width: 50px; height: 50px;" @click="playPauseFunction"></b-icon>
+        </div>
+      </b-col>
+    </b-row>
+    <b-row align-h="center">
+      <b-col cols="8" align-self="center">
+        <p class="text-break">
+          <span v-for="(time, index) in timestamp" :key="index" class="cursor" @dblclick="deleteWord(index)">
+             <span 
+              :class="{ 'text-danger': time.start_time <= currentTime && time.end_time >= currentTime,
+                'font-weight-bolder': time.start_time <= currentTime && time.end_time >= currentTime }"
+               v-if="!time.strikethrough">
+              {{time.text}}
+             </span>
+            <del v-else>{{time.text}}</del>
+          </span>
+        </p>
+      </b-col>
+    </b-row>
+  </div>
+</template>
+
+<script>
+import WaveSurfer from 'wavesurfer.js'
+export default {
+  name: 'Audio',
+  data () {
+    return {
+        play: false,
+        url: '',
+        jsonUrl: '',
+        wavesurfer: null,
+        currentTime: null,
+        audioFile: null,
+        timestamp: []
+      }
+    },
+    methods: {
+      playPauseFunction() {
+        this.play = !this.play
+        this.wavesurfer.playPause()
+      },
+      initialize () {
+        this.wavesurfer = WaveSurfer.create({
+            container: this.$refs.waveform,
+            waveColor: 'violet',
+            progressColor: 'red',
+            barHeight: 2
+        });
+        this.wavesurfer.load(this.url)
+        this.wavesurfer.getVolume()
+        this.wavesurfer.seekTo(0,1)
+        this.wavesurfer.on('audioprocess',() => {
+            this.currentTime = this.wavesurfer.getCurrentTime()
+            this.wavesurfer.params.container.style.opacity = 0.9;
+        });
+      },
+      getJsonFile (event) {
+        this.wavesurfer = null
+        var reader = new FileReader();
+        reader.onload = this.onReaderLoad;
+        reader.readAsText(event.target.files[0])
+      },
+      onReaderLoad(event){
+        var obj = JSON.parse(event.target.result);
+        obj.sections.forEach(async section => {
+        await this.getString(section.words)
+        });
+        this.url = obj.meta.url
+        this.initialize()
+      },
+      getString (arrayForString) {
+        for (let i = 0; i < arrayForString.length; i++) {
+            this.timestamp.push(arrayForString[i])
+        }
+      },
+      deleteWord (index) {
+        this.timestamp[index].strikethrough = true
+      }
+    }
+}
+</script>
+
+<style>
+.cursor {
+  cursor: pointer;
+}
+</style>
